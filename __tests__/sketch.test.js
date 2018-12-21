cdf13bcebd29c4f90ebbaff424fe8c103cdff9f7
hansfpc <hansfpc@outlook.com>
2018-12-21 14:46:43 -0300

    Add new PROD func

--- __tests__/sketch.test.js
+++ __tests__/sketch.test.js
@@ -1,7 +1,10 @@
-const sum = require('../sketch')
+const { sum, prod } = require('../sketch')
 
 describe('Sketch tests', () => {
   it('should adds 1+2 and return 3', () => {
     expect(sum(1, 2)).toBe(3)
+  })
+  it('should 7*5 and return 35', () => {
+    expect(prod(7, 5)).toBe(35)
   })
 })
